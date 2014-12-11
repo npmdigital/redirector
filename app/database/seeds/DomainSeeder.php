8618fdc7496d93ad19b2c08e8c6c35d381576807
Josh Justice <josh@need-bee.com>
2014-12-11 08:27:10 -0500

    updates for latest laravel template; Squashed commit of the following:
    commit 3db5c5498c335c8422e1a9578058bc918961ce60
    Author: Josh Justice <josh@need-bee.com>
    Date:   Thu Dec 11 08:26:53 2014 -0500
    
        adds grunt
    
    commit c92258fb47dd608a575502ab923ae2ad84ac7de0
    Author: Josh Justice <josh@need-bee.com>
    Date:   Thu Dec 11 08:23:59 2014 -0500
    
        removes unused tests
    
    commit 75c008d66d0aef7a2e672b1fdf30c65871158f57
    Author: Josh Justice <josh@need-bee.com>
    Date:   Thu Dec 11 08:07:47 2014 -0500
    
        updates seed urls
    
    commit 06e12f946df6a13a5c3142963b9e1c38fe10346e
    Author: Josh Justice <josh@need-bee.com>
    Date:   Thu Dec 11 08:06:46 2014 -0500
    
        switches db connection to use .env file
    
    commit aa99bc96e6b4b08fa0dfff6f5a7eddfcfd786953
    Author: Josh Justice <josh@need-bee.com>
    Date:   Thu Dec 11 08:01:00 2014 -0500
    
        removes commands dir

--- app/database/seeds/DomainSeeder.php
+++ app/database/seeds/DomainSeeder.php
@@ -4,31 +4,31 @@ use \NpmWeb\Redirector\Models\Domain;
 
 class DomainSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
         DB::table('domains')->delete();
 
-		$domain = new Domain();
-		$domain->fill([
-			'name' => 'redirect.northpoint.org',
-			'redirect_url' => 'http://northpoint.org',
-			'status' => 302,
-		]);
-		$domain->save();
+        $domain = new Domain();
+        $domain->fill([
+            'name' => 'local.redirect.npmweb.org',
+            'redirect_url' => 'http://npmweb.org',
+            'status' => 302,
+        ]);
+        $domain->save();
 
-		$domain = new Domain();
-		$domain->fill([
-			'name' => 'inactive.northpoint.org',
-			'redirect_url' => 'http://northpoint.org',
-			'status' => 302,
-			'active' => false,
-		]);
-		$domain->save();
-	}
+        $domain = new Domain();
+        $domain->fill([
+            'name' => 'local.inactive.npmweb.org',
+            'redirect_url' => 'http://npmweb.org',
+            'status' => 302,
+            'active' => false,
+        ]);
+        $domain->save();
+    }
 
 }
