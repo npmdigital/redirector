fdaccae66f4d4a8a1ecab8e2e67a27aeb0da6ae2
Josh Justice <josh@need-bee.com>
2015-02-26 16:50:08 -0500

    queries hits for just a certain time range

--- app/database/queries/hitcount.sql
+++ app/database/queries/hitcount.sql
@@ -1,5 +1,7 @@
 SELECT d.name, COUNT(*) AS hits
 FROM domains d
 LEFT OUTER JOIN hits h ON d.id = h.domain_id
+WHERE d.created_at <= '2015-01-01'
+AND (h.created_at >= '2015-01-01' AND h.created_at <= '2015-02-01')
 GROUP BY d.name
 ORDER BY hits DESC;
