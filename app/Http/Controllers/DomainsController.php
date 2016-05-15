d1ce750ecf092ae0fb224315f1c384347456ff9d
Brian Weaver <brian.weaver@northpoint.org>
2016-05-15 14:57:22 -0400

    Clean up and code style

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -11,27 +11,26 @@ class DomainsController extends Controller
 
     public function index()
     {
-        $domain = Domain::matching($_SERVER['SERVER_NAME'])
-            ->first();
+        $domain = Domain::matching($_SERVER['SERVER_NAME'])->first();
+
         if (!$domain || !$domain->active) {
             abort(404);
         }
 
         // log hit
-        $data = array(
+        $data = [
             'domain_id' => $domain->id,
             'server_values' => json_encode($_SERVER),
             'path' => $_SERVER['REQUEST_URI'],
-        );
+        ];
+
         if (isset($_SERVER['HTTP_REFERER'])) {
             $data['referer'] = $_SERVER['HTTP_REFERER'];
         }
+
         Hit::create($data);
 
         // redirect to destination
-        return Redirect::to(
-            $domain->redirect_url,
-            $domain->status
-        );
+        return Redirect::to($domain->redirect_url, $domain->status);
     }
 }
